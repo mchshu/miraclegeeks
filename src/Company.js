587f3e22865d0a1dd525e5d16fe0815680b3e4b5
mchshu <51379901+mchshu@users.noreply.github.com>
2019-06-05 04:00:15 +1000

    Update Company.js

--- src/Company.js
+++ src/Company.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Company() {
-    return <h2>company</h2>;
+    return <h2>company</h2>
+    <p>ASEF is looking to enhance the design, technical components and information structure of ASEF Classroom
+Network’s (ASEF ClassNet) existing website to provide a better platform to connect schools and teachers,
+to involve potential partners and to better reach out to members of the general public interested in AsiaEurope
+school cooperation</p>;
 }
